416ff8ece4e7aa025e86fe3f207df6cc7e6b232d
yuhei1222 <yuhei1222@gmail.com>
2018-11-05 17:30:18 -0600

    Added name
    Report

--- Gibbs sampling.py
+++ Gibbs sampling.py
@@ -1,3 +1,6 @@
+### Name of Members: Weidi Pan, Wen Yuan Yen, Yuhei Koshino ####
+
+
 import numpy, math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,15 +14,15 @@ init_printing()
 ###x and y are set as random variables from the uniform distribution. After x and y are calculated, they are stored in matrix m
 
 
-def gibbs(N,thin, theta):
+def gibbs(N,thin, beta):
     x = np.random.uniform(0,1)
     y = np.random.uniform(0,1)
     m = np.zeros(shape= (N,2))
     for i in range(N):
         U = numpy.random.uniform()
-        m[i] = ((-1/y)*math.log(1-U*(1-math.exp(-theta*y)))),(-1/x)*math.log(1-U*(1-math.exp(-theta*x)))
-        y = (-1/x)*math.log(1-U*(1-math.exp(-theta*x)))
-        x = (-1/y)*math.log(1-U*(1-math.exp(-theta*y)))
+        m[i] = ((-1/y)*math.log(1-U*(1-math.exp(-beta*y)))),(-1/x)*math.log(1-U*(1-math.exp(-beta*x)))
+        y = (-1/x)*math.log(1-U*(1-math.exp(-beta*x)))
+        x = (-1/y)*math.log(1-U*(1-math.exp(-beta*y)))
     return(m)
 
 
