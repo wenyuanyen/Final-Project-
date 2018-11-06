5c0048470155e3d07fbb673a23de3ea406eb3da7
Wen-Yuan Yen <37941479+wenyuanyen@users.noreply.github.com>
2018-11-06 01:37:08 -0600

    Some debuggs

--- Gibbs sampling.py
+++ Gibbs sampling.py
@@ -9,31 +9,38 @@ from sympy import *
 init_printing()
 
 
-
 ###The function defined below includes the Inverse Transform Sampling to generate the samples from the conditional distribution.
 ###x and y are set as random variables from the uniform distribution. After x and y are calculated, they are stored in matrix m
 
 
-def gibbs(N,thin, beta):
+def gibbs(N, thin, B):
     x = np.random.uniform(0,1)
     y = np.random.uniform(0,1)
-    m = np.zeros(shape= (N,2))
+    m = np.zeros(shape= (N,2)) 
     for i in range(N):
-        U = numpy.random.uniform()
-        m[i] = ((-1/y)*math.log(1-U*(1-math.exp(-beta*y)))),(-1/x)*math.log(1-U*(1-math.exp(-beta*x)))
-        y = (-1/x)*math.log(1-U*(1-math.exp(-beta*x)))
-        x = (-1/y)*math.log(1-U*(1-math.exp(-beta*y)))
+        for j in range(thin):
+            U = np.random.uniform()
+            x = (-1/y)*math.log(1-U*(1-math.exp(-B*y)))
+            U = np.random.uniform()
+            y = (-1/x)*math.log(1-U*(1-math.exp(-B*x)) )                      
+        m[i,:] = [x,y]
     return(m)
 
 
-m = gibbs(500,10,5)
-plt.hist([row[0]for row in m])
-print(f"Expection=", numpy.mean([row[0] for row in m]))
+m2 = gibbs(500,10,5)
+plt.hist(m2[:,0])
+print(f"Expection=", np.mean(m2[:,0]))
 
-m = gibbs(5000,10,5)
-plt.hist([row[0] for row in m])
-print(f"Expection=", numpy.mean([row[0] for row in m]))
+m3 = gibbs(5000,10,5)
+plt.hist(m3[:,0])
+print(f"Expection=", np.mean(m3[:,0]))
 
-m = gibbs(50000,10,5)
-plt.hist([row[0] for row in m])
-print(f"Expection=", numpy.mean([row[0] for row in m]))
+m4 = gibbs(50000,10,5)
+plt.hist(m4[:,0])
+print(f"Expection=", np.mean(m4[:,0]))
+
+table = PrettyTable()
+table.title = "Gibb sampling estimate of E(X), B = 5"
+table.add_column('T', [500,5000,50000])
+table.add_column('Estimate', [np.mean(m2[:,0]), np.mean(m3[:,0]), np.mean(m4[:,0])])
+print(table)
